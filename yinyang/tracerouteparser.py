818479b0f8bd5d794bd74979bc0d5bbefa301c14
Rickard Östman <rost@spotify.com>
2015-11-14 17:05:55 +0100

    changed stupid naming

--- yinyang/tracerouteparser.py
+++ yinyang/tracerouteparser.py
@@ -1,10 +1,12 @@
 import urllib2
 import json
 import requests
-
+import logging
 from ripe.atlas.sagan import TracerouteResult
 from Ixp_lan import Ixp_lan
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def findAsn(ip):
     r = requests.get("https://stat.ripe.net/data/network-info/data.json?resource=%s" % ip)
@@ -24,24 +26,28 @@ result['result'] = []
 
 def process(my_traceroute):
     for hop in my_traceroute.hops:
-        this_hop = {}
-        this_hop['id'] = hop.index
-        this_hop['rtt'] = hop.packets[0].rtt
-        this_hop['origin'] = hop.packets[0].origin
-        ixp_object = Ixp_lan()
+        if hop.packets[0].origin:
+            this_hop = {}
+            this_hop['id'] = hop.index
+            this_hop['rtt'] = hop.packets[0].rtt
+            this_hop['origin'] = hop.packets[0].origin
+            ixp_object = Ixp_lan()
 
-        worked, name = ixp_object.check_for_ixp_ip(hop.packets[0].origin)
-        if worked:
-            this_hop['ixp'] = name
+            countrycode, name = ixp_object.check_for_ixp_ip(hop.packets[0].origin)
 
-            try:
-                this_hop['asnumber'] = ixp_object.query_for_ip(hop.packets[0].origin)
-            except:
-                this_hop['asnumber'] = 12345
+            if countrycode:
+                this_hop['ixp'] = name
 
+                try:
+                    this_hop['asn'] = ixp_object.query_for_ip(hop.packets[0].origin)
+                except:
+                    this_hop['asn'] = 12345
+
+            else:
+                this_hop['asn'] = findAsn(hop.packets[0].origin)
+
+            result['result'].append(this_hop)
         else:
-            this_hop['asnumber'] = findAsn(hop.packets[0].origin)
-
-        result['result'].append(this_hop)
+            logger.error('skipped invalid hop')
 
     return result
